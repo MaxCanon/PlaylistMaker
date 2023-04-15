9b6b23e9d3466c895def5e8b1918a0fe010cae2e
Maxim Kanonchyk <bizkitman@yandex.ru>
2023-04-16 00:17:09 +0300

    fixes after review

--- app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -5,7 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class TrackAdapter(private val trackList: ArrayList<Track>) :
+class TrackAdapter(private val trackList: List<Track>) :
     RecyclerView.Adapter<TrackHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackHolder {
         val item: View =
