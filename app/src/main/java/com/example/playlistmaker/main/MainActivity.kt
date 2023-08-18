d80b78b4c69b100e2cefd0c5f4a850789ea6dd56
Bizkit <bizkitman@yandex.by>
2023-08-18 16:20:56 +0300

    19 sprint. fixes

--- app/src/main/java/com/example/playlistmaker/main/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/main/MainActivity.kt
@@ -1,33 +1,28 @@
 package com.example.playlistmaker.main
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.playlistmaker.R
-import com.example.playlistmaker.search.ui.activity.SearchActivity
-import com.example.playlistmaker.media.activity.MediaActivity
-import com.example.playlistmaker.settings.ui.activity.SettingsActivity
+import com.example.playlistmaker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
-        val imageSearch = findViewById<Button>(R.id.icon_search)
-        val imageLibrary = findViewById<Button>(R.id.icon_media_library)
-        val imageSettings = findViewById<Button>(R.id.icon_settings)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
 
-        fun navigateTo(clazz: Class<out AppCompatActivity>) {
-            val intent = Intent(this, clazz)
-            startActivity(intent)
-        }
-        imageSearch.setOnClickListener { navigateTo(SearchActivity::class.java) }
-        imageLibrary.setOnClickListener { navigateTo(MediaActivity::class.java) }
-        imageSettings.setOnClickListener { navigateTo(SettingsActivity::class.java) }
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigation.setupWithNavController(navController)
 
     }
 }
--- app/src/main/java/com/example/playlistmaker/media/fragment/FavoriteTracksFragment.kt
+++ app/src/main/java/com/example/playlistmaker/media/fragment/FavoriteTracksFragment.kt
@@ -19,7 +19,7 @@ class FavoriteTracksFragment() : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentFavoritesTracksBinding.inflate(inflater,container,false)
         return binding.root
     }
