32fe51bacaee6e19cd488af0221edffab2d33ac9
Bizkit <bizkitman@yandex.by>
2023-09-20 15:05:31 +0300

    coroutines

--- app/src/main/java/com/example/playlistmaker/search/domain/interactor/SearchInteractor.kt
+++ app/src/main/java/com/example/playlistmaker/search/domain/interactor/SearchInteractor.kt
@@ -1,10 +1,10 @@
 package com.example.playlistmaker.search.domain.interactor
 
-import com.example.playlistmaker.search.domain.model.NetworkError
 import com.example.playlistmaker.search.domain.model.Track
+import kotlinx.coroutines.flow.Flow
 
 interface SearchInteractor {
-    fun searchTracks(query: String, onSuccess: (List<Track>) -> Unit, onError: (NetworkError) -> Unit)
+    fun searchTracks(expression: String) : Flow<Pair<List<Track>?, String?>>
     fun getHistory(): List<Track>
     fun saveHistory(tracks: List<Track>)
 }
